b37d7cfe1ef27a6ab52c0adba8335ad6eec91191
Asa Neko <nekoa931@gmail.com>
2024-06-26 18:48:26 +0200

    modified:   package-lock.json 	modified:   package.json 	deleted:    src/App.css 	modified:   src/App.jsx 	new file:   src/assets/style.css 	deleted:    src/index.css 	modified:   src/main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import './assets/style.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+function Asa() {
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <body>
+      <header class="header">
+          <div class="box">
+              <h1><span class="sc">#</span><b class="pc"> AsaNeko</b></h1>
+              <h2><span class="tr">##</span> sleepy cat</h2>
+          </div>
+          <img class="img" src="https://github.com/AsaNekoo.png" />
+      </header>
+      <div class="column">
+          <div class="main">
+              <box class="box">
+                  <h1><span class="sc">#</span><b class="pc"> hobbies</b></h1>
+
+                  <h2><span class="sc">-</span><b class="pc"> 3D Modeling</b></h2>
+                  <h2><span class="sc">-</span><b class="pc"> Linux system administration</b></h2>
+                  <h2><span class="sc">-</span><b class="pc"> Virtual Reality</b></h2>
+              </box>
+              <box class="box">
+                  <h1><span class="sc">#</span><b class="pc"> skils</b></h1>
+
+                  <h2><span class="sc">-</span><b class="pc"> Blender, Substance Painter</b></h2>
+                  <h2><span class="sc">-</span><b class="pc"> JS </b><span class="sc2">[nodejs, react]</span></h2>
+                  <h2><span class="tr">-</span><b class="pc"></b></h2>
+              </box>
+          </div>
+          <box class="box">
+              <h1><span class="sc">#</span><b class="pc"> info</b></h1>
+
+              <h2><span class="sc">-</span><b class="pc"> e-mail: nekoa931@gmail.com</b></h2>
+              <h2 class="end"><span class="sc">-</span><b class="pc"> languages: en_US, cs_CZ, ru_RU, uk_UA</b></h2>            
+          </box>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </body>
+  );
 }
 
-export default App
+export default Asa;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import * as ReactDOM from 'react-dom/client';
+import Asa from './App';
+import React from 'react';
 
+// 3. Render your application with `ColorModeScript` and `ChakraProvider`
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+         <Asa />
+    </React.StrictMode>,
+);
